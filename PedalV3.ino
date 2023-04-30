ffcd6aa93052d776f5502ffc99b43e3f61e810e6
Aftershock3995 <121205606+Aftershock3995@users.noreply.github.com>
2023-04-29 20:17:43 -0400

    Update PedalV3.ino
    added serial output for data on start up

--- PedalV3.ino
+++ PedalV3.ino
@@ -1,6 +1,5 @@
 #include <AccelStepper.h>
 #include <Encoder.h>
-#include <FastAccelStepper.h>
 
 // Pedal_1_settings
 #define PEDAL1_STEPS_PER_REV 200
@@ -51,31 +50,26 @@
 #define PEDAL_3_LOAD_CELL_PIN A8
 
 // Create AccelStepper objects for each pedal
-
 AccelStepper pedal1(AccelStepper::DRIVER, PEDAL_1_STEP_PIN, PEDAL_1_DIR_PIN);
 AccelStepper pedal2(AccelStepper::DRIVER, PEDAL_2_STEP_PIN, PEDAL_2_DIR_PIN);
 AccelStepper pedal3(AccelStepper::DRIVER, PEDAL_3_STEP_PIN, PEDAL_3_DIR_PIN);
 
 // Create Encoder objects for each pedal
-
 Encoder pedal1Encoder(PEDAL_1_ENC_A_PIN, PEDAL_1_ENC_B_PIN);
 Encoder pedal2Encoder(PEDAL_2_ENC_A_PIN, PEDAL_2_ENC_B_PIN);
 Encoder pedal3Encoder(PEDAL_3_ENC_A_PIN, PEDAL_3_ENC_B_PIN);
 
 // Create variables to store the current pedal positions
-
 long pedal1Position = 0;
 long pedal2Position = 0;
 long pedal3Position = 0;
 
 // Create variables to store the current load cell values
-
 int pedal1LoadCellValue = 0;
 int pedal2LoadCellValue = 0;
 int pedal3LoadCellValue = 0;
 
 // Function to home the pedals by moving them to the back and front limit switches
-
 void setup()
 {
   // Initialize serial communication
@@ -83,7 +77,6 @@ void setup()
   Serial.begin(9600);
 
   // Home each pedal one at a time idk looks cooler
-
   Serial.println("Homing pedal 1...");
   homePedal1();
   Serial.println("Homing pedal 2...");
@@ -92,7 +85,6 @@ void setup()
   homePedal3();
 
   // Initialize limit switches as inputs
-
   pinMode(PEDAL_1_LIMIT_SWITCH_BACK_PIN, INPUT_PULLUP);
   pinMode(PEDAL_1_LIMIT_SWITCH_FRONT_PIN, INPUT_PULLUP);
   pinMode(PEDAL_2_LIMIT_SWITCH_BACK_PIN, INPUT_PULLUP);
@@ -101,16 +93,95 @@ void setup()
   pinMode(PEDAL_3_LIMIT_SWITCH_FRONT_PIN, INPUT_PULLUP);
 
   // Set maximum speed and acceleration
-
   pedal1.setMaxSpeed(PEDAL1_MAX_SPEED);
   pedal2.setMaxSpeed(PEDAL2_MAX_SPEED);
   pedal3.setMaxSpeed(PEDAL3_MAX_SPEED);
+
+  // Print out pedal 1 settings
+  Serial.println("Pedal 1 settings:");
+  Serial.print("Steps per rev: ");
+  Serial.println(PEDAL1_STEPS_PER_REV);
+  Serial.print("Max speed: ");
+  Serial.println(PEDAL1_MAX_SPEED);
+  Serial.print("Max acceleration: ");
+  Serial.println(PEDAL1_MAX_ACCEL);
+  Serial.print("Max force: ");
+  Serial.println(PEDAL1_MAX_FORCE);
+  Serial.print("Spring constant: ");
+  Serial.println(PEDAL1_SPRING_CONSTANT);
+  Serial.print("Step pin: ");
+  Serial.println(PEDAL_1_STEP_PIN);
+  Serial.print("Dir pin: ");
+  Serial.println(PEDAL_1_DIR_PIN);
+  Serial.print("Encoder A pin: ");
+  Serial.println(PEDAL_1_ENC_A_PIN);
+  Serial.print("Encoder B pin: ");
+  Serial.println(PEDAL_1_ENC_B_PIN);
+  Serial.print("Limit switch back pin: ");
+  Serial.println(PEDAL_1_LIMIT_SWITCH_BACK_PIN);
+  Serial.print("Limit switch front pin: ");
+  Serial.println(PEDAL_1_LIMIT_SWITCH_FRONT_PIN);
+  Serial.print("Load cell pin: ");
+  Serial.println(PEDAL_1_LOAD_CELL_PIN);
+
+  // Print out pedal 2 settings
+  Serial.println("Pedal 2 settings:");
+  Serial.print("Steps per rev: ");
+  Serial.println(PEDAL2_STEPS_PER_REV);
+  Serial.print("Max speed: ");
+  Serial.println(PEDAL2_MAX_SPEED);
+  Serial.print("Max acceleration: ");
+  Serial.println(PEDAL2_MAX_ACCEL);
+  Serial.print("Max force: ");
+  Serial.println(PEDAL2_MAX_FORCE);
+  Serial.print("Spring constant: ");
+  Serial.println(PEDAL2_SPRING_CONSTANT);
+  Serial.print("Step pin: ");
+  Serial.println(PEDAL_2_STEP_PIN);
+  Serial.print("Dir pin: ");
+  Serial.println(PEDAL_2_DIR_PIN);
+  Serial.print("Encoder A pin: ");
+  Serial.println(PEDAL_2_ENC_A_PIN);
+  Serial.print("Encoder B pin: ");
+  Serial.println(PEDAL_2_ENC_B_PIN);
+  Serial.print("Limit switch back pin: ");
+  Serial.println(PEDAL_2_LIMIT_SWITCH_BACK_PIN);
+  Serial.print("Limit switch front pin: ");
+  Serial.println(PEDAL_2_LIMIT_SWITCH_FRONT_PIN);
+  Serial.print("Load cell pin: ");
+  Serial.println(PEDAL_2_LOAD_CELL_PIN);
+
+  // Print out pedal 3 settings
+  Serial.println("Pedal 3 settings:");
+  Serial.print("Steps per rev: ");
+  Serial.println(PEDAL3_STEPS_PER_REV);
+  Serial.print("Max speed: ");
+  Serial.println(PEDAL3_MAX_SPEED);
+  Serial.print("Max acceleration: ");
+  Serial.println(PEDAL3_MAX_ACCEL);
+  Serial.print("Max force: ");
+  Serial.println(PEDAL3_MAX_FORCE);
+  Serial.print("Spring constant: ");
+  Serial.println(PEDAL3_SPRING_CONSTANT);
+  Serial.print("Step pin: ");
+  Serial.println(PEDAL_3_STEP_PIN);
+  Serial.print("Dir pin: ");
+  Serial.println(PEDAL_3_DIR_PIN);
+  Serial.print("Encoder A pin: ");
+  Serial.println(PEDAL_3_ENC_A_PIN);
+  Serial.print("Encoder B pin: ");
+  Serial.println(PEDAL_3_ENC_B_PIN);
+  Serial.print("Limit switch back pin: ");
+  Serial.println(PEDAL_3_LIMIT_SWITCH_BACK_PIN);
+  Serial.print("Limit switch front pin: ");
+  Serial.println(PEDAL_3_LIMIT_SWITCH_FRONT_PIN);
+  Serial.print("Load cell pin: ");
+  Serial.println(PEDAL_3_LOAD_CELL_PIN);
 }
 
 void homePedal1()
 {
   // Home pedal 1
-
   pedal1.setSpeed(-PEDAL1_MAX_SPEED);
   while (digitalRead(PEDAL_1_LIMIT_SWITCH_BACK_PIN) == HIGH)
   {
@@ -128,7 +199,6 @@ void homePedal1()
 void homePedal2()
 {
   // Home pedal 2
-
   pedal2.setSpeed(-PEDAL2_MAX_SPEED);
   while (digitalRead(PEDAL_2_LIMIT_SWITCH_BACK_PIN) == HIGH)
   {
@@ -148,7 +218,6 @@ void homePedal2()
 void homePedal3()
 {
   // Home pedal 3
-
   pedal3.setSpeed(-1000);
   while (digitalRead(PEDAL_3_LIMIT_SWITCH_BACK_PIN) == HIGH)
   {
@@ -172,7 +241,7 @@ int readLoadCellValue(int loadCellPin)
 
 void loop()
 {
-  
+
   // Get the current position of each pedal and print it to serial
   pedal1Position = pedal1.currentPosition();
   Serial.print("Pedal 1 Position: ");
@@ -185,39 +254,33 @@ void loop()
   pedal3Position = pedal3.currentPosition();
   Serial.print("Pedal 3 Position: ");
   Serial.println(pedal3Position);
-  
-  // Read the load cell values for each pedal
 
+  // Read the load cell values for each pedal
   pedal1LoadCellValue = readLoadCellValue(PEDAL_1_LOAD_CELL_PIN);
   pedal2LoadCellValue = readLoadCellValue(PEDAL_2_LOAD_CELL_PIN);
   pedal3LoadCellValue = readLoadCellValue(PEDAL_3_LOAD_CELL_PIN);
 
   // Calculate the force to apply to each pedal based on the load cell value
-
   int pedal1Force = (pedal1LoadCellValue * PEDAL1_SPRING_CONSTANT) / 1024;
   int pedal2Force = (pedal2LoadCellValue * PEDAL2_SPRING_CONSTANT) / 1024;
   int pedal3Force = (pedal3LoadCellValue * PEDAL3_SPRING_CONSTANT) / 1024;
 
   // Move each pedal to the desired position based on the calculated force
-
   pedal1.moveTo(pedal1Position - (pedal1Force / 2));
   pedal2.moveTo(pedal2Position - (pedal2Force / 2));
   pedal3.moveTo(pedal3Position - (pedal3Force / 2));
 
   // Update the current pedal positions
-
   pedal1Position = pedal1.currentPosition();
   pedal2Position = pedal2.currentPosition();
   pedal3Position = pedal3.currentPosition();
 
   // Run the stepper motors
-
   pedal1.run();
   pedal2.run();
   pedal3.run();
 
   // Move each pedal back to its home position if the load cell value drops to zero
-
   if (pedal1LoadCellValue == 0)
   {
     pedal1.moveTo(0);
